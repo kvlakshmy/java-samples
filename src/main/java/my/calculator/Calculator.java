41e9dc1b8b45f2801fdd4705d229a357573f6f59
NarayanaSwamy <narayanaswamy.a@gmail.com>
2019-01-01 11:48:40 +1100

    Added new method

--- src/main/java/my/calculator/Calculator.java
+++ src/main/java/my/calculator/Calculator.java
@@ -4,4 +4,12 @@ public class Calculator {
 	public Integer add(Integer a, Integer b) {
 		return a + b;
 	}
+	
+	public Integer addAll(Integer... num) {
+		int sum = 0;
+		for(int i=0;i<num.length;i++) {
+			sum+=num[i];
+		}
+		return sum;
+	}
 }
